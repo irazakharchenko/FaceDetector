16ecb8ecf5d261c30dcda589ebd9ce4a495a4de7
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-08-23 10:51:31 +0300

    add functions for detecting faces

--- app/src/main/java/com/example/iryna/facedetector/FaceOverlayView.java
+++ app/src/main/java/com/example/iryna/facedetector/FaceOverlayView.java
@@ -2,10 +2,30 @@ package com.example.iryna.facedetector;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.hardware.camera2.params.Face;
+import android.media.FaceDetector;
 import android.util.AttributeSet;
 import android.util.SparseArray;
 import android.view.View;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
+
+import com.google.android.gms.vision.face.Landmark;
+
+import android.graphics.ImageFormat;
+import android.util.Log;
+import android.util.SparseArray;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+
+
 
 import static android.R.attr.bitmap;
 
@@ -18,9 +38,7 @@ public class FaceOverlayView extends View {
     // constructor for viewing object from code (new FaceOverlayView)
     public FaceOverlayView(Context context) {
         this(context, null);
-        public void setBitmap( Bitmap bitmap ) {
-            mBitmap = bitmap;
-        }
+
     }
 
     // constructor for viewing object from .xml
@@ -32,5 +50,82 @@ public class FaceOverlayView extends View {
     public FaceOverlayView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
+    // save the bitmap passed to it
+    public void setBitmap( Bitmap bitmap ) {
+        mBitmap = bitmap;
+        FaceDetector detector = new FaceDetector.Builder( getContext() )
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setMode(FaceDetector.FAST_MODE)
+                .build();
+
+        if (!detector.isOperational()) {
+            //Handle contingency
+            // When user first time use app. It needs to assemble all libraries.
+            // need to check is all libraries we need ready to use.
+        } else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            mFaces = detector.detect(frame);
+            detector.release();
+        }
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+        // check scale of device user use
+        // for show picture that can be seen on different devices
+        if ((mBitmap != null) && (mFaces != null)) {
+            double scale = drawBitmap(canvas);
+            drawFaceBox(canvas, scale);
+        }
+
+    }
+
+
+    private double drawBitmap( Canvas canvas ) {
+        // draw bitmap onto canva and size it appropriately
+        double viewWidth = canvas.getWidth();
+        double viewHeight = canvas.getHeight();
+        double imageWidth = mBitmap.getWidth();
+        double imageHeight = mBitmap.getHeight();
+        double scale = Math.min( viewWidth / imageWidth, viewHeight / imageHeight );
+
+        Rect destBounds = new Rect( 0, 0, (int) ( imageWidth * scale ), (int) ( imageHeight * scale ) );
+        canvas.drawBitmap( mBitmap, null, destBounds, null );
+        return scale;
+    }
+
+    private void drawFaceBox(Canvas canvas, double scale) {
+        // draw rectangles around each face
+
+        //paint should be defined as a member variable rather than
+        //being created on each onDraw request, but left here for
+        //emphasis.
+        Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+
+        float left = 0;
+        float top = 0;
+        float right = 0;
+        float bottom = 0;
+
+        for( int i = 0; i < mFaces.size(); i++ ) {
+            Face face = mFaces.valueAt(i);
+
+            left = (float) ( face.getPosition().x * scale );
+            top = (float) ( face.getPosition().y * scale );
+            right = (float) scale * ( face.getPosition().x + face.getWidth() );
+            bottom = (float) scale * ( face.getPosition().y + face.getHeight() );
+
+            canvas.drawRect( left, top, right, bottom, paint );
+        }
+    }
 }
-}
+
